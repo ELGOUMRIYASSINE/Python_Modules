f717a981edca3aa108957b854c50af73f90d18be
Yassine <yassinegomri2004@email.com>
2026-01-24 10:44:47 +0100

    complete the last exercice ex5 module 02 still need docstrings ...

--- Python_Module_02/ex5/ft_garden_management.py
+++ Python_Module_02/ex5/ft_garden_management.py
@@ -3,7 +3,8 @@ class GardenError(Exception):
 
 
 class PlantError(GardenError):
-    pass
+    def __str__(self):
+        return "Error adding plant: Plant name cannot be empty!\n"
 
 
 class WaterError(GardenError):
@@ -13,51 +14,65 @@ class WaterError(GardenError):
 class GardenManager:
     adding_process = 0
 
-    def __init__(self):
+    def __init__(self, tank):
         self.plants = []
+        self.tank = tank
 
     def add(self, object):
-        if (self.adding_process == 0):
-            print("Adding plants to garden...")
-        if (object.name == ""):
-            raise PlantError("Error adding plant: Plant name cannot be empty!\n")
-        else:
-            self.plants.append(object)
-            print(f"Added {object.name} successfully")
-            self.adding_process += 1
+        try:
+            if (self.adding_process == 0):
+                print("Adding plants to garden...")
+            if (object.name == ""):
+                raise PlantError
+            else:
+                self.plants.append(object)
+                print(f"Added {object.name} successfully")
+                self.adding_process += 1
+        except PlantError as e:
+            print(e)
 
     def water_plants(self):
-        print("Opening watering system")
-        for plant in self.plants:
-            if (not (plant.water >= 1 and plant.water <= 10)):
-                if (plant.water < 1):
-                    raise WaterError(f"Error: Water level"
-                                     f" {plant.water} is too low (min 1)\n")
-                else:
-                    raise WaterError(f"Error: Water level"
-                                     f" {plant.water} is too high (max 10)\n")
-            plant.water += 1
-            print(f"Watering {plant.name} - success")
+        print("Watering plants...")
+        try:
+            if self.tank <= 0:
+                raise GardenError("Error: Not enough water in tank")
+            print("Opening watering system")
+            for plant in self.plants:
+                if self.tank <= 0:
+                    raise GardenError("Error: Not enough water in tank")
+                plant.water += 1
+                self.tank -= 1
+                print(f"Watering {plant.name} - success")
+        except GardenError as e:
+            print(e)
+        finally:
+            print("Closing watering system\n")
 
     def check_health(self):
-        for plant in self.plants:
-            if (not (plant.water >= 1 and plant.water <= 10)):
-                if (plant.water < 1):
-                    raise PlantError(f"Error: Water level"
-                                     f" {plant.water} is too low (min 1)\n")
+        try:
+            print("Checking plant health...")
+            for plant in self.plants:
+                if (not (plant.water >= 1 and plant.water <= 10)):
+                    if (plant.water < 1):
+                        raise WaterError(f"Error: Water level {plant.water}"
+                                         f"is too low(min 1) \n")
+                    else:
+                        raise WaterError(f"Error: Water level {plant.water}"
+                                         f"is too high (max 10)\n")
+                elif (not (plant.sun >= 2 and plant.sun <= 12)):
+                    if (plant.sun < 2):
+                        raise GardenError(f"Error: Sunlight hours "
+                                          f"{plant.sun} is too low (min 2)\n")
+                    else:
+                        raise GardenError(f"Error: Sunlight hours {plant.sun}"
+                                          f"is too high (max 12)\n")
                 else:
-                    raise PlantError(f"Error: Water level"
-                                     f" {plant.water} is too high (max 10)\n")
-            elif (not (plant.sun >= 2 and plant.sun <= 12)):
-                if (plant.sun < 2):
-                    raise PlantError(f"Error: Sunlight hours "
-                                     f"{plant.sun} is too low (min 2)\n")
-                else:
-                    raise PlantError(f"Error: Sunlight hours "
-                                     f"{plant.sun} is too high (max 12)\n")
-            else:
-                print(f"{plant.name}: healthy (water: "
-                      f"{plant.water}, sun: {plant.water})")
+                    print(f"{plant.name}: healthy (water: "
+                          f"{plant.water}, sun: {plant.sun})")
+        except Exception as e:
+            print(e)
+        finally:
+            print("Checking plant health systeme closed")
 
 
 class Plant:
@@ -66,11 +81,29 @@ class Plant:
         self.water = water
         self.sun = sun
 
-print("=== Garden Management System ===\n")
-p1 = Plant("maticha", 10, 4)
-p2 = Plant("dela7", 9, 2)
 
-manager = GardenManager()
-manager.add(p1)
-manager.add(p1)
+def systeme():
+    print("=== Garden Management System ===\n")
+    p1 = Plant("maticha", 0, 4)
+    p2 = Plant("dela7", -10, 12)
 
+    manager = GardenManager(100)
+    manager.add(p1)
+    manager.add(p2)
+    print()
+    manager.water_plants()
+    print()
+    manager.check_health()
+    print()
+
+    try:
+        print("Testing error recovery...")
+        raise GardenError("Not enough water in tank")
+    except GardenError as e:
+        print(f"Caught GardenError: {e}")
+    finally:
+        print("System recovered and continuing...\n")
+    print("Garden management system test complete!")
+
+
+systeme()
